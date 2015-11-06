e837e12290510009d0727e49a79681b0cac2b6e5
Carl Xiong <xiongc05@gmail.com>
2015-11-06 20:37:08 +0800

    Change to dynamic programming approach

--- decode-ways/index.js
+++ decode-ways/index.js
@@ -1,13 +1,21 @@
 // https://leetcode.com/problems/decode-ways/
 
+var cache = {};
+
 var numDecodings = function(s){
-  if (s.length <= 1)
+  if (s.length === 0)
+    return 0;
+  if (s.length === 1)
     return 1;
 
   var headNum = Number( s.substr(0,2) );
 
+  cache[s.substr(1)] = cache[s.substr(1)] || numDecodings(s.substr(1));
+
   if (headNum <= 26){
-    return numDecodings(s.substr(2)) + numDecodings(s.substr(1));
+    cache[s.substr(2)] = cache[s.substr(1)] || numDecodings(s.substr(2));
+
+    return cache[s.substr(1)] + cache[s.substr(2)];
   }
   else{
     return numDecodings(s.substr(1));
